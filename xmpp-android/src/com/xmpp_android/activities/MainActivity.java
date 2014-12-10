f4d77d3d3eb18d0c9d7cf31e8c757494bfd21492
FarminF <farmin.f@gmail.com>
2014-12-10 14:38:01 +0100

    Receive from new session and Disconnect from server implemented

--- xmpp-android/src/com/xmpp_android/activities/MainActivity.java
+++ xmpp-android/src/com/xmpp_android/activities/MainActivity.java
@@ -1,6 +1,5 @@
 package com.xmpp_android.activities;
 
-
 import org.jivesoftware.smack.SmackAndroid;
 import org.jivesoftware.smack.SmackException.NotConnectedException;
 
@@ -15,13 +14,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.xmpp_android.R;
 import com.xmpp_android.settings.Connection_Setting;
 import com.xmpp_android.xmpp.ServiceXMPP;
 
 public class MainActivity extends Activity {
-	
+
 	SharedPreferences sharedPref;
 	String username;
 	String password;
@@ -30,39 +30,42 @@ public class MainActivity extends Activity {
 	ServiceXMPP openFireConnection;
 	Button btnconnectToServer;
 	Button btnSendMessage;
+	Button btnDisconnectFromServer;
 	String AddressedUser;
 	String domain;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		domain = getString(R.string.domain_name);
-		
-		//Required for using aSmack - also add the DNSjava jar fire in library
+
+		// Required for using aSmack - also add the DNSjava jar fire in library
 		SmackAndroid.init(MainActivity.this);
-		//Getting User and Server Configuration from Setting shared Preferences			
+		// Getting User and Server Configuration from Setting shared Preferences
 		getSharedPreference();
-		Log.d("f", domain);
-		//button connect and it's listener
+
+		// button connect and it's listener
 		btnconnectToServer = (Button) findViewById(R.id.btnconnectToServer);
 		btnconnectToServer.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
-				//AGAIN Getting User and Server Configuration from Setting shared Preferences if users has changed it
-				getSharedPreference();				
-				//Connecting to Openfire Server
-				connectToOpenFireServer();			
+
+				// AGAIN Getting User and Server Configuration from Setting
+				// shared Preferences if users has changed it
+				getSharedPreference();
+				// Connecting to Openfire Server
+				connectToOpenFireServer();
+
 			}
 		});
-		
-		//button Send and it's listener
+
+		// button Send and it's listener
 		btnSendMessage = (Button) findViewById(R.id.btnsendmsg);
 		btnSendMessage.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
@@ -75,36 +78,71 @@ public class MainActivity extends Activity {
 				}
 			}
 		});
-	}
-	
-	
 
-	//function which reads parameters that has set in settings
+		// button Disconnect and it's listener
+		btnSendMessage = (Button) findViewById(R.id.btnDisconnectFromServer);
+		btnSendMessage.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				try {
+					disconnectFromOpenFireServer();
+				} catch (NotConnectedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	// End Of OnCreate
+	// ********************************************************************
+
+	// function which reads parameters that has set in settings
 	private void getSharedPreference() {
 		// TODO Auto-generated method stub
 		sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
 		username = sharedPref.getString("textUsername", "");
 		password = sharedPref.getString("textPassword", "");
 		serveraddress = sharedPref.getString("textServerAddress", "");
-		serverport = 5222 ;
-		Log.d("log", username + password + serveraddress + serverport);	 
+		serverport = 5222;
+		Log.d("Settings", username + password + serveraddress + serverport);
 	}
-	
-	//function of connecting to XMPP Server
+
+	// function of connecting to XMPP Server
 	private void connectToOpenFireServer() {
+
 		// TODO Auto-generated method stub
-		openFireConnection = new ServiceXMPP (serveraddress, username , password , domain);
-		openFireConnection.connect();		
+		openFireConnection = new ServiceXMPP(serveraddress, username, password,
+				domain);
+		// openFireConnection.connect();
+		if (openFireConnection.connect())
+
+			Toast.makeText(getApplicationContext(),
+					"you are connected to server", Toast.LENGTH_SHORT).show();
+		else
+			Toast.makeText(getApplicationContext(),
+					"you are NOT connected to server", Toast.LENGTH_SHORT)
+					.show();
+
 	}
-	
-	//function of sending message to addressed user
-	private void sendMessage(String addressedUser2) throws NotConnectedException {
+	//function to Disconnect from openFire
+	protected void disconnectFromOpenFireServer() throws NotConnectedException {
+		// TODO Auto-generated method stub
+		openFireConnection.disconnect();
+	}
+
+	// function of sending message to addressed user
+	private void sendMessage(String addressedUser2)
+			throws NotConnectedException {
 		// TODO Auto-generated method stub
 		openFireConnection.chat(addressedUser2);
 	}
-	
-	// Menu and Setting functions -----------------------------------------------
-	//---------------------------------------------------------------------------
+
+	// Menu and Setting functions
+	// -----------------------------------------------
+	// ---------------------------------------------------------------------------
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -126,7 +164,7 @@ public class MainActivity extends Activity {
 			return true;
 		case R.id.exit:
 			finish();
-            System.exit(0);
+			System.exit(0);
 			return true;
 		default:
 			break;
--- xmpp-android/src/com/xmpp_android/xmpp/ServiceXMPP.java
+++ xmpp-android/src/com/xmpp_android/xmpp/ServiceXMPP.java
@@ -3,23 +3,24 @@ package com.xmpp_android.xmpp;
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ExecutionException;
 
 import org.apache.harmony.javax.security.sasl.SaslException;
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.ChatManager;
+import org.jivesoftware.smack.ChatManagerListener;
 import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.ConnectionConfiguration.SecurityMode;
 import org.jivesoftware.smack.ConnectionListener;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.SmackException.NotConnectedException;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smack.ConnectionConfiguration.SecurityMode;
-import org.jivesoftware.smack.SmackException.NotConnectedException;
 import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 
-import android.R;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -33,6 +34,7 @@ public class ServiceXMPP{
 	private String passwordUser;	
 	private XMPPConnection connection;
 	private String serverDomain;
+	private boolean isConnected=false;
 	
 	public ServiceXMPP(String serverAddress, String loginUser, String passwordUser , String domain){
 		this.serverAddress = serverAddress;
@@ -44,12 +46,13 @@ public class ServiceXMPP{
 	public String logconnected;
 	
 	//Connecting to Server - should call this method in MainActivity
-	public void connect(){
+	public Boolean connect(){
+		Boolean retVal =false;
 		AsyncTask<Void, Void, Boolean> connectionThread = new AsyncTask<Void, Void, Boolean>(){			
+			
 			@Override
 			protected Boolean doInBackground(Void... arg0){
-				boolean isConnected = false;
-
+				isConnected=false;
 				ConnectionConfiguration config = new ConnectionConfiguration(serverAddress , 5222 , serverDomain);
 				config.setReconnectionAllowed(true);
 				config.setSecurityMode(SecurityMode.disabled);
@@ -61,24 +64,51 @@ public class ServiceXMPP{
 			    //XMPPConnection.DEBUG_ENABLED = true;
 
 				try{
-
 					connection.connect();
-
 					isConnected = true;
+					
 				} catch (IOException e){
+					Log.e("error", "IO Exception error");
 				} catch (SmackException e){
+					Log.e("error", "Smack Exception error");
 				} catch (XMPPException e){
+					Log.e("error", "XMPP Exception error");
 				}
-				 if (isConnected = true){
-				 }
-
 				 return isConnected;
 				
 			}
+			@Override
+			protected void onPostExecute(Boolean result) {
+				ChatManager chatmanager = ChatManager.getInstanceFor(connection);
+				chatmanager.addChatListener(
+						new ChatManagerListener() {
+							@Override
+							public void chatCreated(Chat chat, boolean createdLocally)
+							{
+								if (!createdLocally)
+									chat.addMessageListener(new MyMessageListener());
+							}
+						});
+			}
+
 		};		
+		
 		connectionThread.execute();
+
+
+		//Checking if connection was successful or not
+		try {
+			retVal = connectionThread.get();
+			System.out.println(" result is : "+retVal);
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return retVal;
+		
 	}
 	
+	//Sending message to addressed user
 	public void chat(String AddressedUser) throws NotConnectedException {
 		//Create username whom we want to send a message
 		String userToSend = AddressedUser + "@" + serverDomain;
@@ -86,29 +116,27 @@ public class ServiceXMPP{
 		ChatManager chatmanager = ChatManager.getInstanceFor(connection);
 		Chat newChat = chatmanager.createChat(userToSend , new MessageListener() {
 			@Override
-			public void processMessage(Chat arg0, Message arg1) {
+			public void processMessage(Chat chat, Message message	) {
 				// TODO Auto-generated method stub
-				//System.out.println("Received message: " + message);
+				System.out.println("Received message: " + message);
 
 			}
 		});
-
+		
 		try {
 			newChat.sendMessage("Hey android2!");
 		}
 		catch (XMPPException e) {
 			System.out.println("Error Delivering block");
 		}
+		
 	}
-	
+	//login in server
 	private void login(XMPPConnection connection, final String loginUser, final String passwordUser){
 		try{
-			connection.login(loginUser, passwordUser);
-			
-			Presence presence = new Presence(Presence.Type.available);
-			connection.sendPacket(presence);
-			
-			
+			connection.login(loginUser, passwordUser);			
+			setStatus(true);
+
 			} catch (NotConnectedException e){	
 			// If is not connected, a timer is schedule and a it will try to reconnect
 				new Timer().schedule(new TimerTask()
@@ -125,8 +153,16 @@ public class ServiceXMPP{
 		} catch (IOException e){
 		}
 	}
-	
+	//Set Presence or Status of user
+	private void setStatus(boolean available) throws NotConnectedException {
+		// TODO Auto-generated method stub
+		 if(available)
+		        connection.sendPacket(new Presence(Presence.Type.available));
+		    else
+		        connection.sendPacket(new Presence(Presence.Type.unavailable));
+	}
 
+	//listener for keeping connection connect
 	public class XMPPConnectionListener implements ConnectionListener{	
 		@Override
 		public void connected(final XMPPConnection connection){
@@ -146,6 +182,12 @@ public class ServiceXMPP{
 		@Override
 		public void reconnectionSuccessful(){}
 	}
-
+	//Disconnect from server
+	public void disconnect() throws NotConnectedException {
+	    if (connection!=null && connection.isConnected()) {
+	        setStatus(false);
+	        connection.disconnect();
+	    }
+	}
 	
 }
